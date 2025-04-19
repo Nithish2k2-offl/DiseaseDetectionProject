f5d29f37639d57fa65b493c81725a9aaa9d797c4
Nithish <Nithishoffl24@gmail.com>
2025-04-19 15:43:51 +0530

    New Database added..

--- src/client.js
+++ src/client.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://fabgbzoamyrykqdogvuk.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhYmdiem9hbXlyeWtxZG9ndnVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA0MjY4NDAsImV4cCI6MjAyNjAwMjg0MH0.I5yXUCbVLqHcbL6ld72c0vaux30NL99Vy8GsRoF7hmk"
+const supabaseUrl = "https://qdziceidkbnizkesgobr.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkemljZWlka2JuaXprZXNnb2JyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ4Njc0NTUsImV4cCI6MjA2MDQ0MzQ1NX0.RD58crw9HpKlIXgf9IOkXk5DoUXrz6t4393SFbKWlzA"
 export const supabase = createClient(supabaseUrl, supabaseKey)
